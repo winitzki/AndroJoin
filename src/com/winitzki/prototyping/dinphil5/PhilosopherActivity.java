f67e73bf792ed7392e49e4a18b4d35416257c085
Sergei Winitzki <sergei.winitzki@versal.com>
2014-03-05 13:36:11 -0800

    creating layout for test app

--- src/com/winitzki/prototyping/dinphil5/PhilosopherActivity.java
+++ src/com/winitzki/prototyping/dinphil5/PhilosopherActivity.java
@@ -2,16 +2,33 @@ package com.winitzki.prototyping.dinphil5;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class PhilosopherActivity extends Activity {
 
+	TextView tv1, tv2, tv3, tv4, tv5;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_philosopher);
 	}
 
+	@Override
+	public View onCreateView(String name, Context context, AttributeSet attrs) {
+		View v = super.onCreateView(name, context, attrs);
+	
+		tv1 = (TextView)v.findViewById(R.id.tv1);
+		tv2 = (TextView)v.findViewById(R.id.tv2);
+		tv3 = (TextView)v.findViewById(R.id.tv3);
+		tv4 = (TextView)v.findViewById(R.id.tv4);
+		tv5 = (TextView)v.findViewById(R.id.tv5);
+		return v;
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
